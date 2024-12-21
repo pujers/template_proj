54a7c9fbe77d483b5567ad4f43f8d9869ac92252
Puja Sari <puja.resma@gmail.com>
2024-12-21 20:59:54 +0100

    Added

---
+++ templateplay/practice/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class FilterView(TemplateView):
+    template_name = 'practice/filters.html'
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class TagView(TemplateView):
+    template_name = 'practice/tags.html'
